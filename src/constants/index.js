dc287fa1b1aad760f4f7eda75a5e0ded200641cc
Deepanshu Tyagi <29deepanshutyagi.com>
2024-03-17 00:13:28 +0530

    portfolio

---
+++ src/constants/index.js
@@ -0,0 +1,152 @@
+import { meta, shopify, starbucks, tesla } from "../assets/images";
+import {
+    car,
+    contact,
+    css,
+    django,
+    estate,
+    express,
+    git,
+    github,
+    html,
+    javascript,
+    linkedin,
+    mongodb,
+    motion,
+    mui,
+    nextjs,
+    nodejs,
+    pricewise,
+    react,
+    redux,
+    sass,
+    snapgram,
+    summiz,
+    tailwindcss,
+    threads,
+    twitter,
+    typescript
+} from "../assets/icons";
+
+export const skills = [
+    {
+        imageUrl: css,
+        name: "CSS",
+        type: "Frontend",
+    },
+    {
+        imageUrl: express,
+        name: "Express",
+        type: "Backend",
+    },
+    {
+        imageUrl: git,
+        name: "Git",
+        type: "Version Control",
+    },
+    {
+        imageUrl: github,
+        name: "GitHub",
+        type: "Version Control",
+    },
+    {
+        imageUrl: html,
+        name: "HTML",
+        type: "Frontend",
+    },
+    {
+        imageUrl: javascript,
+        name: "JavaScript",
+        type: "Frontend",
+    },
+    {
+        imageUrl: mongodb,
+        name: "MongoDB",
+        type: "Database",
+    },
+    {
+        imageUrl: django,
+        name: "Django",
+        type: "Backend",
+    },
+   
+   
+    {
+        imageUrl: nodejs,
+        name: "Node.js",
+        type: "Backend",
+    },
+    {
+        imageUrl: react,
+        name: "React",
+        type: "Frontend",
+    },
+    {
+        imageUrl: redux,
+        name: "Redux",
+        type: "State Management",
+    },
+    {
+        imageUrl: sass,
+        name: "Sass",
+        type: "Frontend",
+    },
+    {
+        imageUrl: tailwindcss,
+        name: "Tailwind CSS",
+        type: "Frontend",
+    },
+    
+];
+
+
+
+export const socialLinks = [
+    {
+        name: 'twitter',
+        iconUrl: twitter,
+        link: 'https://twitter.com/deepanshu_2910',
+    },
+    {
+        name: 'GitHub',
+        iconUrl: github,
+        link: 'https://github.com/29deeapnshutyagi',
+    },
+    {
+        name: 'LinkedIn',
+        iconUrl: linkedin,
+        link: 'https://www.linkedin.com/in/deepanshu-tyagi-2122ab268/',
+    }
+];
+
+export const projects = [
+    {
+        iconUrl: pricewise,
+        theme: 'btn-back-red',
+        name: 'CryptoVerse',
+        description: 'Developed a web application for displaying concept & algorithms of crypto currencies, including changes in values & cost, advantages, disadvantages and financial future of Cryptocurrency.',
+        link: 'https://cryptoverse-96eee9.netlify.app/',
+    },
+   
+];
+export const service = [
+    {
+
+        theme: 'btn-back-red',
+        name: 'Web Development',
+
+    },
+    {
+
+        theme: 'btn-back-red',
+        name: 'backend Development',
+
+    },
+   
+    {
+
+        theme: 'btn-back-red',
+        name: 'Content Creation',
+
+    },
+]
